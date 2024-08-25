a217d0de7ef7d29003fc52f417158db0471b0460
Mikhail Datoo <mikhail@datoos.com>
2024-08-25 11:47:33 +0000

    fix: binary classification support

--- kaggle_classification/models/classification_model.py
+++ kaggle_classification/models/classification_model.py
@@ -4,16 +4,14 @@ from typing import Any, Dict
 
 import lightning.pytorch as pl
 import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sn
 import torch
 from torch import nn, optim
 from torchmetrics import (
     Accuracy,
-    CatMetric,
     ConfusionMatrix,
     F1Score,
     Precision,
+    PrecisionRecallCurve,
     Recall,
 )
 from torchvision.utils import make_grid
@@ -50,13 +48,13 @@ class ClassificationModel(pl.LightningModule):
         self.optimiser = optimiser
         self.optimiser_scheduler = optimiser_scheduler
 
-        self.val_acc = Accuracy(task="multiclass", num_classes=num_classes)
-        self.val_pre = Precision(task="multiclass", num_classes=num_classes)
-        self.val_rec = Recall(task="multiclass", num_classes=num_classes)
-        self.val_f1 = F1Score(task="multiclass", num_classes=num_classes)
-        self.val_confusion = ConfusionMatrix(task="multiclass", num_classes=num_classes)
-        self.val_outputs = CatMetric()
-        self.val_labels = CatMetric()
+        task = "multiclass" if num_classes > 2 else "binary"
+        self.val_acc = Accuracy(task=task, num_classes=num_classes)
+        self.val_pre = Precision(task=task, num_classes=num_classes)
+        self.val_rec = Recall(task=task, num_classes=num_classes)
+        self.val_f1 = F1Score(task=task, num_classes=num_classes)
+        self.val_confusion = ConfusionMatrix(task=task, num_classes=num_classes)
+        self.val_pr_curve = PrecisionRecallCurve(task=task, num_classes=num_classes)
 
     def training_step(self, batch: torch.Tensor, batch_idx: int) -> torch.Tensor:  # pylint: disable=arguments-differ
         """Calculate and log metrics/loss to tensorboard.
@@ -89,12 +87,11 @@ class ClassificationModel(pl.LightningModule):
         _, labels = torch.max(targets, 1)
 
         outputs = self.model(images)
-        _, predictions = torch.max(outputs.data, 1)
 
         loss = self.criterion(outputs, targets)
         self.log("val_loss", loss, on_step=False, on_epoch=True)
 
-        self.val_acc.update(predictions, labels)
+        self.val_acc.update(outputs, labels)
         self.log("val_accuracy", self.val_acc, on_step=False, on_epoch=True)
 
         self.val_pre.update(outputs, labels)
@@ -106,40 +103,24 @@ class ClassificationModel(pl.LightningModule):
         self.val_f1.update(outputs, labels)
         self.log("val_f1", self.val_f1, on_step=False, on_epoch=True)
 
-        self.val_confusion.update(predictions, labels)
-        self.val_outputs.update(outputs)
-        self.val_labels.update(labels)
+        self.val_confusion.update(outputs, labels)
+        self.val_pr_curve.update(outputs, labels)
 
     def on_validation_epoch_end(self) -> None:
         """Log confusion matrix and PR curve."""
         if self.logger:
-            confusion_matrix = self.val_confusion.compute().detach().cpu().numpy().astype(int)  # type: ignore[func-returns-value]
-
             plt.figure(figsize=(10, 7))
-            figure = sn.heatmap(pd.DataFrame(confusion_matrix), cmap="mako").get_figure()
+            figure, _ = self.val_confusion.plot(add_text=False)
             plt.close(figure)
             self.logger.experiment.add_figure("val_confusion", figure, self.current_epoch)  # type: ignore[attr-defined]
 
-            val_probs = torch.softmax(self.val_outputs.compute(), 1)
-            val_labels = self.val_labels.compute()
-
-            for image_class in range(val_probs.shape[1]):
-                self.logger.experiment.add_pr_curve(  # type: ignore[attr-defined]
-                    f"val_{image_class}",
-                    val_labels == image_class,
-                    val_probs[:, image_class],
-                    self.current_epoch,
-                )
-            self.logger.experiment.add_pr_curve(  # type: ignore[attr-defined]
-                "val_micro",
-                torch.concat([val_labels == image_class for image_class in range(val_probs.shape[1])]),  # type: ignore[misc]
-                val_probs.transpose(0, 1).flatten(),
-                self.current_epoch,
-            )
+            plt.figure(figsize=(10, 7))
+            figure, _ = self.val_pr_curve.plot()
+            plt.close(figure)
+            self.logger.experiment.add_figure("val_pr_curve", figure, self.current_epoch)  # type: ignore[attr-defined]
 
         self.val_confusion.reset()
-        self.val_outputs.reset()
-        self.val_labels.reset()
+        self.val_pr_curve.reset()
 
     def configure_optimizers(  # type: ignore[override]
         self,
